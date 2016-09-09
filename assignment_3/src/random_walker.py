717e841029420f7c1d3fd5b1ae3e94c7dadc2515
Po-Jen <berln579@gmail.com>
2016-09-09 22:11:01 +0800

    test laserscan callback behavior

--- assignment_3/src/random_walker.py
+++ assignment_3/src/random_walker.py
@@ -3,20 +3,27 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
-#def callback(data):
+flag = 0;
+
+def callback(data):
+    print "angle_min: " + str(data.angle_min)
+    print "angle_max: " + str(data.angle_max)
+    global flag
+    flag = flag + 1;
 
 def random_walker():
     pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
-    #sub = rospy.Subscriber('/scan', LaserScan, callback)
+    sub = rospy.Subscriber('/scan', LaserScan, callback)
     rospy.init_node('turtlebot_random_exploration_node')
     rate = rospy.Rate(10)
 
     cmd = Twist()
 
     while not rospy.is_shutdown():
-        cmd.linear.x = 0.1
+        cmd.linear.x = -0.1
 
         pub.publish(cmd)
+        print flag
         rate.sleep()
 
 
